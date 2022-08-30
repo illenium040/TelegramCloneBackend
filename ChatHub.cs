29836688e8a4e594f26d9abef337b56cb2fc0ca2
illenium040 <illenium040@mail.ru>
2022-08-30 14:23:42 +0300

    Добавьте файлы проекта.

---
+++ ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace TelegramCloneBackend
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+        public async Task BroadcastChartData(string data, string connectionId) =>
+            await Clients.Client(connectionId).SendAsync("BroadCast", data);
+        public string GetConnectionId() => Context.ConnectionId;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Builder;
+using TelegramCloneBackend;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR();
+builder.Services.AddCors(o => {
+    o.AddDefaultPolicy(builder =>
+    {
+        builder
+        .SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseCors();
+app.MapHub<ChatHub>("hubs/notifications");
+
+
+app.Run();
